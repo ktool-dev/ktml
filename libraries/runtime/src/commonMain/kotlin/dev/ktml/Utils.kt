08990367d2d2149d3136b14e13573b36edb76219
Aaron Freeman <aaron@freeman.zone>
2025-10-28 23:45:11 -0500

    removed unused function

--- libraries/runtime/src/commonMain/kotlin/dev/ktml/Utils.kt
+++ libraries/runtime/src/commonMain/kotlin/dev/ktml/Utils.kt
@@ -1,8 +1,5 @@
 package dev.ktml
 
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
-
 internal fun encodeHtml(value: String?) = if (value.isNullOrBlank()) {
     value
 } else {
@@ -16,18 +13,4 @@ internal fun encodeHtml(value: String?) = if (value.isNullOrBlank()) {
             else -> it
         }
     }.joinToString("")
-}
-
-@OptIn(ExperimentalUuidApi::class)
-inline fun <reified T : Any> String?.toType(): T? = this?.let {
-    when (T::class) {
-        String::class -> this as T
-        Int::class -> toInt() as T
-        Long::class -> toLong() as T
-        Float::class -> toFloat() as T
-        Double::class -> toDouble() as T
-        Boolean::class -> toBoolean() as T
-        Uuid::class -> Uuid.parse(this) as T
-        else -> error("No conversion for type: ${T::class}")
-    }
 }
