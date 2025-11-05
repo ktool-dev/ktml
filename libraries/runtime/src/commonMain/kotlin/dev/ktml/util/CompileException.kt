adb2a7c1dcc862b7fde6d3b95f602b3aa42cb4ed
Aaron Freeman <aaron@freeman.zone>
2025-11-05 01:09:46 -0600

    fix: adding exeption wrapper registry

--- libraries/runtime/src/commonMain/kotlin/dev/ktml/util/CompileException.kt
+++ libraries/runtime/src/commonMain/kotlin/dev/ktml/util/CompileException.kt
@@ -1,5 +1,10 @@
 package dev.ktml.util
 
+import dev.ktml.Content
+import dev.ktml.KtmlRegistry
+import dev.ktml.TagDefinition
+import dev.ktml.templates.writeCompileException
+
 
 data class CompilerError(val message: String, val filePath: String, val line: Int = 0, val column: Int = 0) {
     fun toTemplateMessage() = "[$filePath] $message"
@@ -7,3 +12,21 @@ data class CompilerError(val message: String, val filePath: String, val line: In
 
 class CompileException(val errors: List<CompilerError>) :
     Exception("Compilation failed:\n${errors.joinToString("\n") { it.toTemplateMessage() }}")
+
+class CompileExceptionRegistry(val exception: CompileException) : KtmlRegistry {
+    override val tags: List<TagDefinition> = listOf()
+
+    private class FakeMap(val exception: CompileException) : AbstractMap<String, Content>() {
+        override val entries: Set<Map.Entry<String, Content>> = emptySet()
+        override fun get(key: String): Content = {
+            this["exception"] = exception
+            writeCompileException()
+        }
+    }
+
+    override val templates: Map<String, Content> = FakeMap(exception)
+
+    fun printException() {
+        exception.printStackTrace()
+    }
+}
