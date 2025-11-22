e0c105f6339e33d6bd3753a6df39a9fcf95829eb
Aaron Freeman <aaron@freeman.zone>
2025-11-21 23:44:09 -0600

    Updating build stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,30 +16,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "ktml"
 
-library("runtime")
-library("dev-mode")
-library("generator")
+val projectTypeDirs = listOf("libraries", "applications", "integrations")
 
-integration("gradle")
-integration("maven")
-integration("spring")
-integration("ktor")
-integration("javalin")
-integration("http4k")
-
-application("example-javalin")
-application("example-ktor")
-application("example-spring")
-application("example-http4k")
-application("example-ktor-native")
-application("example-js")
-application("example-wasm-js")
-
-fun library(name: String) = includeType("libraries", name)
-fun integration(name: String) = includeType("integrations", name)
-fun application(name: String) = includeType("applications", name)
-
-fun includeType(dir: String, name: String) {
-    include(name)
-    project(":$name").projectDir = File("$dir/$name")
+projectTypeDirs.forEach { type ->
+    file(type).list().forEach { project ->
+        include(project)
+        project(":$project").projectDir = File("$type/$project")
+    }
 }
