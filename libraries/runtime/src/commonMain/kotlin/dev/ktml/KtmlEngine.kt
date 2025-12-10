15ad79edb09debc00fa147453803a915f7ad8b15
Aaron Freeman <aaron@freeman.zone>
2025-12-09 23:11:43 -0600

    fix: adding hasPage to KtmlEngine

--- libraries/runtime/src/commonMain/kotlin/dev/ktml/KtmlEngine.kt
+++ libraries/runtime/src/commonMain/kotlin/dev/ktml/KtmlEngine.kt
@@ -3,13 +3,17 @@ package dev.ktml
 import dev.ktml.templates.DefaultKtmlRegistry
 
 class KtmlEngine(ktmlRegistry: KtmlRegistry) {
-    private val ktmlRegistry = ktmlRegistry.join(DefaultKtmlRegistry)
+    private val ktmlRegistry: KtmlRegistry = ktmlRegistry.join(DefaultKtmlRegistry)
+
+    private fun lookup(path: String): Content? = ktmlRegistry[path.removePrefix("/")]
 
     suspend fun writePage(context: Context, path: String) {
-        ktmlRegistry[path.removePrefix("/")]?.invoke(context) ?: error("No page found for path: $path")
+        lookup(path)?.invoke(context) ?: error("No page found for path: $path")
     }
 
     suspend fun renderPage(path: String, model: Map<String, Any> = mapOf()) = StringContentWriter().also {
         writePage(Context(it, model), path)
     }.toString()
+
+    fun hasPage(path: String): Boolean = lookup(path) != null
 }
